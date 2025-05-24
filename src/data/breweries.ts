2db08b0cee83259d04e92d21e8b86ff055462dd8
Chelsey <chelsey.madsen@gmail.com>
2025-05-24 18:55:43 -0500

    Update brewery data source

--- src/data/breweries.ts
+++ src/data/breweries.ts
@@ -1,172 +1,18 @@
-import { Location } from '../types';
+import { Location, VerifiedBrewery } from '../types';
+import breweriesJson from './mke_breweries_all_21.json';
 
-export const breweries: Omit<Location, 'id'>[] = [
-  {
-    name: "Milwaukee Brewing Company",
-    type: "both",
-    coordinates: [42.997142, -87.904026],
-    yelpLink: "https://www.yelp.com/biz/milwaukee-brewing-company-milwaukee-2",
-    address: "2335 S Kinnickinnic Ave, Milwaukee, WI 53207",
-    rating: 4
-  },
-  {
-    name: "Sugar Maple",
-    type: "outdoor",
-    coordinates: [42.996197, -87.903406],
-    yelpLink: "https://www.yelp.com/biz/sugar-maple-milwaukee",
-    address: "441 E Lincoln Ave, Milwaukee, WI 53207",
-    rating: 4
-  },
-  {
-    name: "1840 Brewing Company",
-    type: "outdoor",
-    coordinates: [42.994653, -87.899987],
-    yelpLink: "https://www.yelp.com/biz/1840-brewing-company-milwaukee",
-    address: "342 E Ward St, Milwaukee, WI 53207",
-    rating: 4
-  },
-  {
-    name: "Burnhearts",
-    type: "outdoor",
-    coordinates: [42.998591, -87.903309],
-    yelpLink: "https://www.yelp.com/biz/burnhearts-milwaukee",
-    address: "2599 S Logan Ave, Milwaukee, WI 53207",
-    rating: 4
-  },
-  {
-    name: "Torzala Brewing Company",
-    type: "indoor",
-    coordinates: [42.996128, -87.901768],
-    yelpLink: "https://www.yelp.com/biz/torzala-brewing-company-milwaukee",
-    address: "2018 S 1st St Suite 207, Milwaukee, WI 53207",
-    rating: 4
-  },
-  {
-    name: "Enlightened Brewing Company",
-    type: "indoor",
-    coordinates: [42.99598, -87.901493],
-    yelpLink: "https://www.yelp.com/biz/enlightened-brewing-company-milwaukee",
-    address: "2020 S Allis St, Milwaukee, WI 53207",
-    rating: 4
-  },
-  {
-    name: "Amorphic Beer",
-    type: "indoor",
-    coordinates: [43.088182, -87.903224],
-    yelpLink: "https://www.yelp.com/biz/amorphic-beer-milwaukee",
-    address: "3700 N Fratney St, Milwaukee, WI 53212",
-    rating: 4
-  },
-  {
-    name: "Black Husky Brewing Co",
-    type: "indoor",
-    coordinates: [43.075317, -87.899681],
-    yelpLink: "https://www.yelp.com/biz/black-husky-brewing-milwaukee",
-    address: "909 E Locust St, Milwaukee, WI 53212",
-    rating: 4
-  },
-  {
-    name: "Broken Bat Brewing Co",
-    type: "indoor",
-    coordinates: [43.024304, -87.910271],
-    yelpLink: "https://www.yelp.com/biz/broken-bat-brewing-company-milwaukee",
-    address: "135 E Pittsburgh Ave, Milwaukee, WI 53204",
-    rating: 4
-  },
-  {
-    name: "Central Waters Brewing",
-    type: "outdoor",
-    coordinates: [43.043202, -87.926727],
-    yelpLink: "https://www.yelp.com/biz/central-waters-brewing-co-milwaukee",
-    address: "1037 W Juneau Ave, Milwaukee, WI 53233",
-    rating: 4
-  },
-  {
-    name: "Component Brewing",
-    type: "indoor",
-    coordinates: [42.996046, -87.901694],
-    yelpLink: "https://www.yelp.com/biz/component-brewing-company-milwaukee",
-    address: "2018 S 1st St #169, Milwaukee, WI 53207",
-    rating: 4
-  },
-  {
-    name: "Dead Bird Brewing Company",
-    type: "outdoor",
-    coordinates: [43.051095, -87.916613],
-    yelpLink: "https://www.yelp.com/biz/dead-bird-brewing-company-milwaukee",
-    address: "1726 N 5th St, Milwaukee, WI 53212",
-    rating: 4
-  },
-  {
-    name: "Eagle Park Brewing Co",
-    type: "both",
-    coordinates: [43.048799, -87.896087],
-    yelpLink: "https://www.yelp.com/biz/eagle-park-brewing-company-milwaukee",
-    address: "823 E Hamilton St, Milwaukee, WI 53202",
-    rating: 4
-  },
-  {
-    name: "Gathering Place Brewing",
-    type: "indoor",
-    coordinates: [43.093029, -87.896363],
-    yelpLink: "https://www.yelp.com/biz/gathering-place-brewing-company-milwaukee",
-    address: "811 E Vienna Ave, Milwaukee, WI 53212",
-    rating: 4
-  },
-  {
-    name: "Good City Brewing Co",
-    type: "both",
-    coordinates: [43.057252, -87.885015],
-    yelpLink: "https://www.yelp.com/biz/good-city-brewing-milwaukee",
-    address: "2108 N Farwell Ave, Milwaukee, WI 53202",
-    rating: 4
-  },
-  {
-    name: "Hacienda Beer Co",
-    type: "indoor",
-    coordinates: [43.060586, -87.882462],
-    yelpLink: "https://www.yelp.com/biz/hacienda-beer-co-milwaukee",
-    address: "2018 E North Ave, Milwaukee, WI 53202",
-    rating: 4
-  },
-  {
-    name: "Indeed Brewing Company",
-    type: "outdoor",
-    coordinates: [43.025303, -87.913546],
-    yelpLink: "https://www.yelp.com/biz/indeed-brewing-company-milwaukee",
-    address: "530 S 2nd St, Milwaukee, WI 53204",
-    rating: 4
-  },
-  {
-    name: "Lakefront Brewery",
-    type: "both",
-    coordinates: [43.052014, -87.900291],
-    yelpLink: "https://www.yelp.com/biz/lakefront-brewery-milwaukee",
-    address: "1872 N Commerce St, Milwaukee, WI 53212",
-    rating: 4
-  },
-  {
-    name: "New Barons Brewing Co-op",
-    type: "indoor",
-    coordinates: [42.996106, -87.901731],
-    yelpLink: "https://www.yelp.com/biz/new-barons-brewing-cooperative-milwaukee",
-    address: "2018 S 1st St #170, Milwaukee, WI 53207",
-    rating: 4
-  },
-  {
-    name: "Third Space Brewing",
-    type: "outdoor",
-    coordinates: [43.033351, -87.930983],
-    yelpLink: "https://www.yelp.com/biz/third-space-brewing-milwaukee",
-    address: "1505 W St Paul Ave, Milwaukee, WI 53233",
-    rating: 4
-  },
-  {
-    name: "Venture Brew Co",
-    type: "both",
-    coordinates: [43.061031, -87.987584],
-    yelpLink: "https://www.yelp.com/biz/venture-brew-co-milwaukee",
-    address: "5519 W North Ave, Milwaukee, WI 53210",
-    rating: 4
-  }
-];
+// Convert JSON data to Location format
+const convertBreweryData = (brewery: VerifiedBrewery): Omit<Location, 'id'> => ({
+  name: brewery.name,
+  type: brewery.tags.includes('Indoor') && brewery.tags.includes('Outdoor') 
+    ? 'both' 
+    : brewery.tags.includes('Indoor') 
+      ? 'indoor' 
+      : 'outdoor',
+  coordinates: [brewery.latitude, brewery.longitude],
+  yelpLink: brewery.yelp,
+  address: brewery.address,
+  rating: brewery.pawRating
+});
+
+export const breweries: Omit<Location, 'id'>[] = breweriesJson.map(convertBreweryData);
