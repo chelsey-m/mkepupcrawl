c9ba3807ac01ebd423c5257444edec2f166a2357
Chelsey <chelsey.madsen@gmail.com>
2025-05-24 18:32:08 -0500

    Update brewery types to indoor

--- src/data/breweries.ts
+++ src/data/breweries.ts
@@ -3,66 +3,58 @@ import { Location } from '../types';
 export const breweries: Omit<Location, 'id'>[] = [
   {
     name: "Amorphic Beer",
-    type: "both",
+    type: "indoor",
     coordinates: [43.0789, -87.9026],
     yelpLink: "https://www.amorphicbeer.com/",
     address: "3700 N Fratney St, Milwaukee, WI",
     rating: 4
   },
   {
-    name: "Perspective Brewing",
-    type: "both",
-    coordinates: [43.0167, -88.0073],
-    yelpLink: "https://www.yelp.com/biz/perspective-brewing-west-allis",
-    address: "7508 W Greenfield Ave, West Allis, WI",
-    rating: 4
-  },
-  {
     name: "Black Husky Brewing",
-    type: "both",
+    type: "indoor",
     coordinates: [43.0714, -87.8997],
     yelpLink: "https://blackhuskybrewing.com/",
     address: "909 E Locust St, Milwaukee, WI",
     rating: 4
   },
   {
-    name: "Gathering Place Brewing",
-    type: "both",
-    coordinates: [43.0784, -87.8947],
-    yelpLink: "https://www.gatheringplacebrewing.com/",
-    address: "811 E Vienna Ave, Milwaukee, WI",
-    rating: 4
-  },
-  {
-    name: "Component Brewing",
-    type: "both",
-    coordinates: [43.0178, -87.9129],
-    yelpLink: "https://www.facebook.com/ComponentBrewing/",
-    address: "2018 S 1st St #169, Milwaukee, WI",
-    rating: 4
-  },
-  {
-    name: "New Barons Brewing Co-op",
-    type: "both",
-    coordinates: [43.0178, -87.9129],
-    yelpLink: "https://newbaronsbrewing.com/",
-    address: "2018 S 1st St #170, Milwaukee, WI",
-    rating: 4
-  },
-  {
     name: "Broken Bat Brewing Co",
-    type: "both",
+    type: "indoor",
     coordinates: [43.0241, -87.9066],
     yelpLink: "https://brokenbatbrewery.com/",
     address: "135 E Pittsburgh Ave, Milwaukee, WI",
     rating: 4
   },
   {
+    name: "Component Brewing",
+    type: "indoor",
+    coordinates: [43.0178, -87.9129],
+    yelpLink: "https://www.facebook.com/ComponentBrewing/",
+    address: "2018 S 1st St #169, Milwaukee, WI",
+    rating: 4
+  },
+  {
     name: "Enlightened Brewing Co",
-    type: "both",
+    type: "indoor",
     coordinates: [43.0169, -87.9129],
     yelpLink: "https://enlightenedbeer.com/",
     address: "2020 S Allis St, Milwaukee, WI",
+    rating: 4
+  },
+  {
+    name: "Gathering Place Brewing",
+    type: "indoor",
+    coordinates: [43.0784, -87.8947],
+    yelpLink: "https://www.gatheringplacebrewing.com/",
+    address: "811 E Vienna Ave, Milwaukee, WI",
+    rating: 4
+  },
+  {
+    name: "New Barons Brewing Co-op",
+    type: "indoor",
+    coordinates: [43.0178, -87.9129],
+    yelpLink: "https://newbaronsbrewing.com/",
+    address: "2018 S 1st St #170, Milwaukee, WI",
     rating: 4
   },
   {
