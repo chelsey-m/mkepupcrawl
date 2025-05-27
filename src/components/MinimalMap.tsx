adf2aec9ea84ce46521c61b7b1c35280d72fca56
Chelsey <chelsey.madsen@gmail.com>
2025-05-27 16:27:54 -0500

    Create minimal debug map component

---
+++ src/components/MinimalMap.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+
+const breweryIcon = new Icon({
+  iconUrl: '/brewery-icon.svg',
+  iconSize: [32, 32],
+  iconAnchor: [16, 32]
+});
+
+const MinimalMap: React.FC = () => {
+  return (
+    <MapContainer 
+      center={[43.052402, -87.900836]} 
+      zoom={18} 
+      style={{ height: '100vh', width: '100vw' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker 
+        position={[43.052402, -87.900836]}
+        icon={breweryIcon}
+      >
+        <Popup>Lakefront Brewery</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MinimalMap;
